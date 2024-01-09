0b6bcd8c434bca8a0cc0aed314c12f46b9afb559
Edwin2345 <112472516+Edwin2345@users.noreply.github.com>
2024-01-09 17:48:14 -0500

    Update TodoServiceImpl.java

--- backend/src/main/java/net/project/todo/service/impl/TodoServiceImpl.java
+++ backend/src/main/java/net/project/todo/service/impl/TodoServiceImpl.java
@@ -13,6 +13,7 @@ import net.project.todo.repository.UserRepository;
 import net.project.todo.service.TodoService;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
